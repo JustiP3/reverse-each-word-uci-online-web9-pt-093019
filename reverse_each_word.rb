50ca9d83e1ea71de75c7e3887cda4cf323677144
Justin Phillips <justinphil02@yahoo.com>
2019-10-11 03:14:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,14 @@ def reverse_each_word(string)
   index = 0 
   array.each do |word|
     array[index] = word.reverse 
-    index += 1 
-    
+    index += 1
   end 
-  
+string = ""
+index = 0
+while index < array.length do 
+string << array [index]
+string << " "
+index += 1 
+end 
+return string 
 end 
