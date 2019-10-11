5a99e21a78463e24c420743a7cd3b2ebfc07fac2
Justin Phillips <justinphil02@yahoo.com>
2019-10-11 03:19:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,26 @@
 def reverse_each_word(string)
+ 
+  #convert string to array 
   array = string.split(" ")
+  
+  #reverse each string element of the array 
   index = 0 
   array.each do |word|
     array[index] = word.reverse 
     index += 1
-  end 
+  end
+  
+  #reinitialize index and string to 0 
 string = ""
 index = 0
+
+#shovel each element of the array into the string 
 while index < array.length do 
 string << array [index]
-string << " "
+#add a space if it is not the last element in the array  
+  if index + 1 != array.length 
+    string << " "
+  end 
 index += 1 
 end 
 return string 
